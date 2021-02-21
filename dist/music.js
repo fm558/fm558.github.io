80f798e4ef669a19ca74bb607cfd4cbffcbb4531
fm558 <anlu@vista.aero>
2021-02-21 16:47:13 +0800

    Site updated: 2021-02-21 16:46:53

---
+++ dist/music.js
@@ -0,0 +1,43 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: 'Welcome To New York',
+        artist: 'Taylor Swift',
+        url: 'https://cdn.jsdelivr.net/gh/app318/image/img/20210131160010.mp3',
+        cover: 'https://cdn.jsdelivr.net/gh/app318/image/img/20210131160321.jpg',
+      },
+      {
+        name: 'Everlasting Truth',
+        artist: 're:plus',
+        url: 'https://cdn.jsdelivr.net/gh/app318/image/img/05.+Everlasting+Truth.mp3',
+        cover: 'https://cdn.jsdelivr.net/gh/app318/image/img/20210131155245.jpg',
+      },
+      {
+        name: 'How You Get The Girl',
+        artist: 'Taylor Swift',
+        url: 'https://cdn.jsdelivr.net/gh/app318/image/img/20210131160041.mp3',
+        cover: 'https://cdn.jsdelivr.net/gh/app318/image/img/20210131160347.jpg',
+      },
+      {
+        name: '梦童话(Instrument)',
+        artist: '灰澈',
+        url: 'https://cdn.jsdelivr.net/gh/app318/image/img/20210131160720.mp3',
+        cover: 'https://cdn.jsdelivr.net/gh/app318/image/img/20210131160644.jpg',
+      },
+      {
+        name: 'Hana',
+        artist: '灰澈',
+        url: 'https://cdn.jsdelivr.net/gh/app318/image/img/20210131161640.mp3',
+        cover: 'https://cdn.jsdelivr.net/gh/app318/image/img/20210131161737.jpg',
+      },
+      {
+        name: '小清新',
+        artist: '朱主爱',
+        url: 'https://cdn.jsdelivr.net/gh/app318/image/img/20210131162057.mp3',
+        cover: 'https://cdn.jsdelivr.net/gh/app318/image/img/20210131162134.jpg',
+      }
+    ]
+});
